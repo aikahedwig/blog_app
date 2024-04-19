a4d99faf76534e8da91dfa099adae071c7bf5b85
aikahed <aika@LAPTOP-2NQC9N65>
2024-04-19 23:32:17 +0900

    デプロイ通貨

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,7 +49,7 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
- server "18.181.166.156",
+ server "18.179.35.85",
    user: "ec2-user",
    roles: %w{web app},
    ssh_options: {
