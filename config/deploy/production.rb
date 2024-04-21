26498e4b102fd2f57a10b736554c97357720233e
aikahed <aika@LAPTOP-2NQC9N65>
2024-04-21 12:40:12 +0900

    2回目のデプロイ

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,7 +49,7 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
- server "18.179.35.85",
+ server "54.178.57.5",
    user: "ec2-user",
    roles: %w{web app},
    ssh_options: {
